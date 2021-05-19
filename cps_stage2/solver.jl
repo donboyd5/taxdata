41d37fecc760aa8b19daedd1223c36655e885b67
Don Boyd <donboyd5@gmail.com>
2021-05-19 09:01:54 -0400

    Tulip options

--- cps_stage2/solver.jl
+++ cps_stage2/solver.jl
@@ -17,6 +17,7 @@ function Solve_func(year, tol)
 	model = Model(Tulip.Optimizer)  # djb
 	set_optimizer_attribute(model, "OutputLevel", 1)  # 0=disable output (default), 1=show iterations
 	set_optimizer_attribute(model, "Threads", 10)  # 1=default; Tulip is single-threaded but linear algebra back ends may use multiple threads
+	JuMP.set_optimizer_attribute(jump_model, "IPM_IterationsLimit", 500)  # default 100
 
 	N = size(A1)[2]
 
@@ -35,6 +36,7 @@ function Solve_func(year, tol)
 
 	optimize!(model)
 	termination_status(model)
+	@printf "Objective = %.4f\n" objective_value(model)
 
 	r_vec = value.(r)
 	s_vec = value.(s)
