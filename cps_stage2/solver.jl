5aadc1a3a9660d1b46fb53b502d8ad3f34c68d0e
Don Boyd <donboyd5@gmail.com>
2021-05-19 12:09:51 -0400

    WIP

--- cps_stage2/solver.jl
+++ cps_stage2/solver.jl
@@ -1,4 +1,4 @@
-using JuMP, Cbc, NPZ, Tulip
+using JuMP, Cbc, NPZ, Tulip  # add Tulip
 using Printf
 
 function Solve_func(year, tol)
@@ -12,13 +12,14 @@ function Solve_func(year, tol)
 	A2 = array["A2"]
 	b = array["b"]
 
+	# Replace these next 2 lines with these 3 lines
 	# model = Model(Cbc.Optimizer)
 	# set_optimizer_attribute(model, "logLevel", 1)
-	# set_optimizer_attribute(model, "autoScale", "on")  # djb
 	model = Model(Tulip.Optimizer)  # djb
 	set_optimizer_attribute(model, "OutputLevel", 1)  # 0=disable output (default), 1=show iterations
-	# set_optimizer_attribute(model, "Threads", 10)  # 1=default; Tulip is single-threaded but linear algebra back ends may use multiple threads
 	set_optimizer_attribute(model, "IPM_IterationsLimit", 100)  # default 100
+
+    # set_optimizer_attribute(model, "Threads", 10)  # 1=default; Tulip is single-threaded but linear algebra back ends may use multiple threads
 
 	N = size(A1)[2]
 
@@ -37,7 +38,12 @@ function Solve_func(year, tol)
 
 	optimize!(model)
 	termination_status(model)
-	@printf "Objective = %.4f\n" objective_value(model)
+	# solution_summary(model, verbose=true)
+	# @printf "Objective = %.4f\n" objective_value(model)
+
+	# add these 2 lines to see final objective function
+	st = termination_status(model)
+	println("Termination status: $st")
 
 	r_vec = value.(r)
 	s_vec = value.(s)
